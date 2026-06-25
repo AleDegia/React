e41065e564da07d9ce835853bdd82cae292697c8
aledegia <degia.ale@gmail.com>
2026-06-25 13:57:54 +0200

    nested components + css applicato

--- 03_primoProgettino/pizza-menu/src/index.js
+++ 03_primoProgettino/pizza-menu/src/index.js
@@ -1,16 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header/>
+      <Menu/>
+      <Footer/>
     </div>
   );
+}
+
+function Header() {
+  
+  return <h1>Fast React Pizza</h1>
+}
+function Menu() {
+  return(
+    <main className="menu">
+      <h2>Out menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  )
+}
+function Footer(){
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  /*if (hour >= openHour && hour <= closeHour)
+    alert("We're currently open!");
+  else
+    alert("Sorry we're closed");*/
+  return(
+    <footer className="footer">{new Date().toLocaleTimeString} We are currently open</footer>
+  );
+  //traduzione in javascript del jsx che fa Babel.. (uguale a return <footer>We're currently open!</footer>;)
+   /*return React.createElement(
+    "footer",
+    null,
+    "We're currently open!"
+  );*/
 }
 
 //componente che mostro piu volte inserendolo in quello principale che viene displayato con root.render (App)
@@ -22,7 +56,7 @@ function Pizza() {
         alt="Pizza spinaci"
       />
 
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
 
       <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
     </div>
