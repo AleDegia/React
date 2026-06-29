71b604a3dcd360590f98490d6435dd6b2b8902b9
aledegia <degia.ale@gmail.com>
2026-06-29 13:00:56 +0200

    Props

--- 03_primoProgettino/pizza-menu/src/index.js
+++ 03_primoProgettino/pizza-menu/src/index.js
@@ -16,13 +16,23 @@ function Header() {
   
   return <h1>Fast React Pizza</h1>
 }
+//passo al componente Pizza le props ingredient, photoName e price
 function Menu() {
   return(
     <main className="menu">
       <h2>Out menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza    
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      /> 
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+    />
     </main>
   )
 }
@@ -39,27 +49,20 @@ function Footer(){
   return(
     <footer className="footer">{new Date().toLocaleTimeString} We are currently open</footer>
   );
-  //traduzione in javascript del jsx che fa Babel.. (uguale a return <footer>We're currently open!</footer>;)
-   /*return React.createElement(
-    "footer",
-    null,
-    "We're currently open!"
-  );*/
 }
 
 //componente che mostro piu volte inserendolo in quello principale che viene displayato con root.render (App)
-function Pizza() {
+function Pizza(props) {
   return (
+  <div className="pizza">
+    <img src={props.photoName} alt={props.name} />
+
     <div>
-      <img
-        src="pizzas/spinaci.jpg"
-        alt="Pizza spinaci"
-      />
-
-      <h3>Pizza Spinaci</h3>
-
-      <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price + 3}</span>
     </div>
+  </div>
   );
 }
 
@@ -73,5 +76,12 @@ root.render(
   </React.StrictMode>
 );
 
-// React before 18
-// ReactDOM.render(<App />, document.getElementById("root"));
+/*
+In function Menu() {} sto riutilizzando lo stesso componente Pizza due volte,
+passando info diverse. È esattamente come chiamare una funzione due volte con parametri diversi.
+è React che la chiama ogni volta che trova <Pizza /> nel JSX.
+
+è come se pizza nel jsx fosse la chiamata al metodo coi vari parametri che gli passo, 
+mentre il componente function Pizza(props) fossa la definizione del metodo
+*/
+
