b164c2d060b0dfb27191d441338935197917d95a
aledegia <degia.ale@gmail.com>
2026-07-01 18:12:09 +0200

    useState

--- 05_steps/steps/src/App.js
+++ 05_steps/steps/src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 
 const messages = [
   "Learn React ✨",
@@ -7,12 +7,19 @@ const messages = [
 ];
 
 function App() { 
- const step = 1;
+ const [step, setStep] = useState(1);   //con useState creo variabile di stato a cui do valore iniziale 1 a step, e setStep è la funzione che aggiorna il valore di step
 
  function handlePrevious() {
-  alert("Previous button clicked");
+  setStep((prevStep) => prevStep - 1);
+  //alert("Previous button clicked");
  }
 
+ function handleNext() {
+  setStep((prevStep) => prevStep + 1);
+  //alert("Next button clicked");
+ }
+
+ //classe active mette colore viola al numero
  return (
   <div className="steps">
     <div className="numbers">
@@ -21,7 +28,7 @@ function App() {
       <div className={`${step >= 3 ? "active" : ""}`}>3</div>
     </div>
 
-    <p className="message">{messages[step - 1]}</p>
+    <p className="message">Step {step}: {messages[step - 1]}</p>
 
     <div className="buttons">
       <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
@@ -29,7 +36,7 @@ function App() {
         Previous 
       </button>
       <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={() =>alert("Next button clicked")}>
+              onClick={handleNext}>
         Next
       </button>
     </div>
@@ -38,3 +45,48 @@ function App() {
 }
 
 export default App;
+
+
+/*
+useState è una funzione che ti permette di aggiungere lo stato a un componente funzionale.
+useState restituisce un array: [step, setStep]
+
+Con la destrutturazione di JavaScript:
+  const [step, setStep] = useState(1);
+
+equivale a scrivere (semplificando):
+
+  const state = useState(1);
+
+const step = state[0];
+const setStep = state[1];
+
+step → il valore dello stato
+setStep → la funzione per modificarlo
+
+
+Const:
+
+const in JavaScript non significa che il valore non possa cambiare, 
+ma che il riferimento alla variabile non può essere riassegnato.
+tu non modifichi mai direttamente step.
+
+Non fai:
+
+step = step + 1; // ❌ Errore
+
+Fai invece:
+
+setStep(prevStep => prevStep + 1);
+
+Quando chiami setStep, React:
+
+salva il nuovo valore dello stato;
+riesegue il componente (App);
+durante la nuova esecuzione: const [step, setStep] = useState(1);
+
+step non vale più 1, ma ad esempio 2.
+
+Quindi ogni render crea una nuova costante step.
+Non è la stessa costante che cambia: è una nuova costante creata ad ogni render.
+*/
